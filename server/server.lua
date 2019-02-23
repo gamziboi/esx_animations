1289e7a91140cf0a7fbebb9173716271499127ee
gamz <37119123+gamziboi@users.noreply.github.com>
2019-02-23 22:29:40 +0100

    no more errors

--- server/server.lua
+++ server/server.lua
@@ -1,29 +1,33 @@
 ESX = nil
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
-AddEventHandler("playerConnecting", function(name)
-    local identifier = GetPlayerIdentifiers(source)[1]
-    local result = MySQL.Sync.fetchScalar("SELECT * FROM favoriteanimation WHERE identifier = @identifier", {['@identifier'] = identifier})
+AddEventHandler('esx:playerLoaded', function(source)
+	local src = source
+	local xPlayer = ESX.GetPlayerFromId(src)
+    local result = MySQL.Async.fetchAll("SELECT * FROM `favoriteanimation` WHERE `identifier` = @identifier", {['@identifier'] = xPlayer.identifier})
     if not result then
-	MySQL.Sync.execute("INSERT INTO favoriteanimation (`identifier`) VALUES (@identifier)",{['@identifier'] = identifier})
+	MySQL.Sync.execute("INSERT IGNORE INTO favoriteanimation (`identifier`) VALUES (@identifier)",{['@identifier'] = xPlayer.identifier})
     end
 end)
 
 RegisterServerEvent('esx_animations:newFavorite')
 AddEventHandler('esx_animations:newFavorite', function(lib, dict, scenario)
-	local identifier = GetPlayerIdentifiers(source)[1]
+	local src = source
+	local xPlayer = ESX.GetPlayerFromId(src)
 	MySQL.Async.execute("UPDATE favoriteanimation SET lib=@lib,animation=@animation,scenario=@scenario WHERE identifier=@identifier", 
 		{
-			['@identifier'] = identifier, ['@lib'] = lib, ['@animation'] = dict, ['@scenario'] = scenario
+			['@identifier'] = xPlayer.identifier, ['@lib'] = lib, ['@animation'] = dict, ['@scenario'] = scenario
 		}
 	)
 end)
 
 ESX.RegisterServerCallback('esx_animations:getFavorite', function(source, cb)
-	local identifier = GetPlayerIdentifiers(source)[1]
+	local src = source
+	local xPlayer = ESX.GetPlayerFromId(src)
+
 		MySQL.Async.fetchAll("SELECT * FROM `favoriteanimation` WHERE `identifier` = @identifier",
 		{
-			['@identifier'] = identifier
+			['@identifier'] = xPlayer.identifier
 		}, function(result)
 		local data = {
 			lib			= result[1].lib,
